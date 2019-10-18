2ad805baa1b6be2faa13b92ada36836e629d1243
OtavioLMS <otaviolmsantos@hotmail.com>
2019-10-18 11:14:38 -0300

    Correção e melhorias

--- sketch.js
+++ sketch.js
@@ -3,46 +3,43 @@ var lista = [];
 var size;
 
 function setup() {
-      createCanvas(window.innerWidth, 600);
-      size = (window.innerWidth - 11) / 10;
-      
-      for(let i = 0; i < 10; i++){
-            lista.push(-random(50,255));
-      }
-      
-      frameRate(1);
+    createCanvas(window.innerWidth, window.innerHeight);
+    size = (window.innerWidth - 11) / 10;
+
+    for(let i = 0; i < 10; i++){
+        lista.push(-random(50,255));
+    }
+
+    frameRate(1);
 }
 
 function draw() {
-      showItens();
-      bubbleSort();
+    showItens();
+    bubbleSort();
 }
 
 function bubbleSort(){
-      var swapped = false;
-      do {
-            swapped = false;
-            for(let i = 0; i < 9; i++){
-                  showItens(i);
-                  setTimeout(function(){
-                        if(lista[i] < lista[i+1]){
-                              let aux = lista[i+1];
-                              lista[i+1] = lista[i];
-                              lista[i] = aux;
-                              swapped = true;
-                        }
-                  });
-            }
-      } while (swapped);
+    var swapped = false;
+    do {
+        swapped = false;
+        for(let i = 0; i < 9; i++){
+            showItens(i);
+            setTimeout(function(){
+                if(lista[i] < lista[i+1]){
+                    let aux = lista[i+1];
+                    lista[i+1] = lista[i];
+                    lista[i] = aux;
+                    swapped = true;
+                }
+            });
+        }
+    } while (swapped);
 }
 
-function showItens(it){
-      background(0);
-      for(item in lista){
-            fill(-lista[item]);
-            if(it == item){
-                  fill(0, 200, -lista[item]);
-            }
-            rect((size + 2) * item, 600, size, 2*lista[item]);
-      }
+function showItens(i){
+    background(0);
+    for(item in lista){
+        fill(0, 200, -lista[item]);
+        rect((size + 2) * item, window.innerHeight, size, 2*lista[item]);
+    }
 }
